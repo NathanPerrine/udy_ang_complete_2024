7534b66494622ab91515dfa4f049077b10e380a8
Nathan Perrine <nathan.perrine@gmail.com>
2024-07-17 09:51:29 -0400

    tasks started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,24 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedId = 'u1'
+
+  get selectedUserName() {
+    return this.users.find((user) => user.id === this.selectedId);
+  }
 
   onSelectUser(id: string) {
-    console.log('selected user with id: ' + id);
+    this.selectedId = id
   }
 }
